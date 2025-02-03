6178a2d7407df6d384962ff6bc510a4b3828c3c4
Ananducn <anandu.cn112@gmail.com>
2025-02-03 16:09:49 +0530

    Fix Netlify routing issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        services: path.resolve(__dirname, 'services.html'),
+        contact: path.resolve(__dirname, 'contact.html'),
+      }
+    }
+  }
+});
